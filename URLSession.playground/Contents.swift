c785fbca5cd8cc21d89c8747c4c7d47a5c2786f4
Vitaliy <vmolokov59@gmail.com>
2023-03-22 18:11:03 +0300

    created default request

--- URLSession.playground/Contents.swift
+++ URLSession.playground/Contents.swift
@@ -1,1 +1,34 @@
+import Foundation
+
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    
+    guard let url = urlRequest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        // Вывод ошибки, если имеется
+        if let error = error {
+            print("Ошибка:", error)
+        }
+        
+        // Вывод кода ответа от сервера
+        if let response = response as? HTTPURLResponse {
+            let statusCode = response.statusCode
+            switch statusCode {
+            case 200:
+                if let data = data, let dataAsString = String(data: data, encoding: .utf8) {
+                    print("Данные, пришедшие с сервера:\n", dataAsString)
+                }
+            case 400...499:
+                print("Ошибка клиента, код ответа:", statusCode)
+            case 500...526:
+                print("Ошибка сервера, код ответа:", statusCode)
+            default:
+                print("Неизвестная ошибка, код ответа:", statusCode)
+            }
+        }
+    }.resume()
+}
+
+getData(urlRequest: "https://api.spacexdata.com/v5/launches/latest")
+
 
